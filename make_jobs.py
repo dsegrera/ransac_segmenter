f1fa3d34597b114d213a383a7bb224a723719d37
Daniel Segrera <segrera.d@gmail.com>
2019-09-26 21:38:05 -0600

    first commit

--- make_jobs.py
+++ make_jobs.py
@@ -12,15 +12,15 @@ with open('jobs/template.sh') as f:
 
 dirs = [flnm for flnm in os.listdir('full_1930_set') if os.path.isdir('full_1930_set/' + flnm)]
 nums = [flnm for flnm in dirs]
-out_dirs = [join('./out', flnm) for flnm in dirs]
+outs_dirs = [join('./outs', flnm) for flnm in dirs]
 dirs = [join('./full_1930_set', flnm) for flnm in dirs]
-existing_out_dirs = [flnm for flnm in os.listdir('out') if os.path.isdir('out/' + flnm)]
+existing_outs_dirs = [flnm for flnm in os.listdir('outs') if os.path.isdir('outs/' + flnm)]
 
 for i in range(len(dirs)):
     first_line =''
-    if not (nums[i] in existing_out_dirs):
-        first_line = 'mkdir ' + out_dirs[i] + '\n'
-    command = first_line + 'python ./segmenter.py ./data/template.jpg ./data/template.json ' + dirs[i] + ' ' + out_dirs[i]
+    if not (nums[i] in existing_outs_dirs):
+        first_line = 'mkdir ' + outs_dirs[i] + '\n'
+    command = first_line + 'python ./segmenter.py ./data/template.jpg ./data/template.json ' + dirs[i] + ' ' + outs_dirs[i]
     sh = template + command
     with open('jobs/' + nums[i] + '.sh', 'w+') as f:
         f.write(sh)
